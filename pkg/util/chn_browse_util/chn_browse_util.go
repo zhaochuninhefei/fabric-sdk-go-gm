3916bd69145f584ef6e51e801e9c96ed06220528
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-06-30 10:23:21 +0800

    重构chn_browse_util，重命名区块头哈希相关变量与字段名

--- pkg/util/chn_browse_util/chn_browse_util.go
+++ pkg/util/chn_browse_util/chn_browse_util.go
@@ -56,8 +56,7 @@ type BlockInfoWithTx struct {
 }
 
 func (t *BlockInfoWithTx) ToString() string {
-	result := fmt.Sprintf("区块编号: %d, 交易数量: %d, 区块哈希: %s, 前区块哈希: %s,\n\t\t交易集合:\n",
-		t.BlockNum, t.TransCnt, t.BlockHash, t.PreBlockHash)
+	result := fmt.Sprintf("%s,\n\t\t交易集合:\n", t.BlockInfoBasic.ToString())
 	for _, t := range t.TransactionInfos {
 		result = result + "\t\t" + t.ToString() + "\n"
 	}
@@ -70,15 +69,15 @@ func (t *BlockInfoWithTx) GetBasicInfo() *BlockInfoBasic {
 
 // 区块基础信息(区块编号、区块哈希、前区块哈希、区块内交易数量)
 type BlockInfoBasic struct {
-	BlockNum     uint64 // 区块编号
-	BlockHash    string // 区块哈希(16进制字符串)
-	PreBlockHash string // 前一区块哈希(16进制字符串)
-	TransCnt     uint64 // 区块内交易数量
+	BlockNum           uint64 // 区块编号
+	BlockHeaderHash    string // 区块头哈希(16进制字符串)
+	PreBlockHeaderHash string // 前区块头哈希(16进制字符串)
+	TransCnt           uint64 // 区块内交易数量
 }
 
 func (t *BlockInfoBasic) ToString() string {
-	result := fmt.Sprintf("区块编号: %d, 交易数量: %d, 区块哈希: %s, 前区块哈希: %s",
-		t.BlockNum, t.TransCnt, t.BlockHash, t.PreBlockHash)
+	result := fmt.Sprintf("区块编号: %d, 交易数量: %d, 区块头哈希: %s, 前区块头哈希: %s",
+		t.BlockNum, t.TransCnt, t.BlockHeaderHash, t.PreBlockHeaderHash)
 	return result
 }
 
@@ -142,9 +141,9 @@ type BrowseChannelConfig struct {
 	//  BlockCountLimit默认值为0，此时BrowseChannel浏览的区块数量无限制。
 	BlockCountLimit uint64
 	// 上回区块哈希
-	//  BrowseLimitType值为1时，BrowseChannel浏览的区块向前不超过且不包括LastBlockHash对应的区块。
-	//  LastBlockHash默认值为空。BrowseLimitType值为1时，LastBlockHash不可为空。
-	LastBlockHash string
+	//  BrowseLimitType值为1时，BrowseChannel浏览的区块向前不超过且不包括LastBlockHeaderHash对应的区块。
+	//  LastBlockHeaderHash默认值为空。BrowseLimitType值为1时，LastBlockHeaderHash不可为空。
+	LastBlockHeaderHash string
 	// 上回区块编号
 	//  BrowseLimit值为2时，BrowseChannel浏览的区块向前不超过且不包括LastBlockNum对应的区块。
 	//  LastBlockNum默认值为0。
@@ -166,58 +165,91 @@ func BrowseChannel(ledgerClient *ledger.Client) (*ChannelInfo, error) {
 //  入参: config 浏览参数
 //  返回: ChannelInfo
 func BrowseChannelWithConfig(ledgerClient *ledger.Client, config *BrowseChannelConfig) (*ChannelInfo, error) {
+	// 检查浏览参数
 	if config == nil {
 		return nil, fmt.Errorf("no config(*BrowseChannelConfig)")
 	}
 	browseLimitType := config.BrowseLimitType
+	// 检查浏览上限类型
 	if browseLimitType < 0 || browseLimitType > 2 {
 		return nil, fmt.Errorf("not supported browseLimitType")
 	}
-	lastBlockHash, _ := hex.DecodeString(config.LastBlockHash)
-
+	var lastBlockHeaderHash []byte
+	if browseLimitType == 1 {
+		// BrowseLimitType值为1时，LastBlockHeaderHash不可为空。
+		if config.LastBlockHeaderHash == "" {
+			return nil, fmt.Errorf("lastBlockHeaderHash is empty")
+		}
+		var err error
+		lastBlockHeaderHash, err = hex.DecodeString(config.LastBlockHeaderHash)
+		// LastBlockHeaderHash解码失败
+		if err != nil {
+			return nil, err
+		}
+	}
+	// 查询当前最新区块链信息
 	blockChainInfo, err := ledgerClient.QueryInfo()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get blockInfo: %s", err)
 	}
+	// 初始化通道情报
 	channelInfo := &ChannelInfo{
 		BlockHeight: blockChainInfo.BCI.Height,
 	}
-
+	// 交易总数，起始值为0
 	var total uint64 = 0
-	curBlockHash := blockChainInfo.BCI.CurrentBlockHash
+	// 当前区块头哈希，起始值为最新区块头哈希
+	curBlockHeaderHash := blockChainInfo.BCI.CurrentBlockHash
+	// 区块情报(包含内部交易情报)集合
 	blockInfoWithTxs := []*BlockInfoWithTx{}
+	// 交易情报集合
 	transactionInfos := []*TransactionInfo{}
+	// 区块基础信息集合
 	blockBasicInfos := []*BlockInfoBasic{}
+	// 区块数量，起始值为0
 	var blockCnt uint64 = 0
+	// 区块编号，起始值为当前区块链高度
+	curBlockNum := channelInfo.BlockHeight
 	for {
+		// 区块数量+1
 		blockCnt++
+		// 区块编号-1
+		curBlockNum--
 		// 当浏览参数为使用区块数量限制时，检查区块数量是否已超过区块数量上限。
 		// BlockCountLimit为0时表示没有区块数量限制。
 		if browseLimitType == 0 && config.BlockCountLimit > 0 && config.BlockCountLimit <= blockCnt {
 			break
 		}
 		// 当浏览参数为使用前回区块哈希限制时，检查本次遍历的区块哈希
-		if browseLimitType == 1 && bytes.Equal(curBlockHash, lastBlockHash) {
+		if browseLimitType == 1 && bytes.Equal(curBlockHeaderHash, lastBlockHeaderHash) {
 			break
 		}
-		block, err := ledgerClient.QueryBlockByHash(curBlockHash)
+		// 当浏览参数为使用前回区块编号限制时，检查本次遍历的区块编号
+		if browseLimitType == 2 && config.LastBlockNum == curBlockNum {
+			break
+		}
+		// 使用当前区块头哈希查询当前区块
+		block, err := ledgerClient.QueryBlockByHash(curBlockHeaderHash)
 		if err != nil {
 			return nil, fmt.Errorf("failed to QueryBlockByHash: %s", err)
 		}
-		// 当浏览参数为使用前回区块编号限制时，检查本次遍历的区块编号
-		if browseLimitType == 2 && config.LastBlockNum == block.Header.Number {
-			break
-		}
-		blockInfo, err := UnmarshalBlockData(block, curBlockHash)
+		// 反序列化当前区块
+		blockInfo, err := UnmarshalBlockData(block, curBlockHeaderHash)
 		if err != nil {
 			return nil, fmt.Errorf("failed to UnmarshalBlockData: %s", err)
 		}
+		// 累加交易数量
 		total += blockInfo.TransCnt
+		// 追加区块情报(包含内部交易情报)
 		blockInfoWithTxs = append(blockInfoWithTxs, blockInfo)
+		// 追加交易情报
 		transactionInfos = append(transactionInfos, blockInfo.TransactionInfos...)
+		// 追加区块基础信息
 		blockBasicInfos = append(blockBasicInfos, blockInfo.GetBasicInfo())
-		curBlockHash = block.Header.PreviousHash
-		if len(curBlockHash) == 0 {
+		// 获取前区块头哈希
+		curBlockHeaderHash = block.Header.PreviousHash
+		// 前区块头哈希为空时，表示已经遍历到第一个区块
+		if len(curBlockHeaderHash) == 0 {
 			break
 		}
 	}
@@ -230,6 +262,7 @@ func BrowseChannelWithConfig(ledgerClient *ledger.Client, config *BrowseChannelC
 
 // UnmarshalBlockData 反序列化Block区块数据。
 //  入参: block 区块数据
+//  入参: curBlockHash 当前区块头哈希
 //  返回: BlockInfo
 func UnmarshalBlockData(block *common.Block, curBlockHash []byte) (*BlockInfoWithTx, error) {
 	// 区块内交易数据集合
@@ -239,10 +272,10 @@ func UnmarshalBlockData(block *common.Block, curBlockHash []byte) (*BlockInfoWit
 	// 准备区块情报
 	blockInfo := &BlockInfoWithTx{
 		BlockInfoBasic: BlockInfoBasic{
-			BlockNum:     block.Header.Number,
-			BlockHash:    hex.EncodeToString(curBlockHash),
-			PreBlockHash: hex.EncodeToString(block.Header.PreviousHash),
-			TransCnt:     uint64(transCnt),
+			BlockNum:           block.Header.Number,
+			BlockHeaderHash:    hex.EncodeToString(curBlockHash),
+			PreBlockHeaderHash: hex.EncodeToString(block.Header.PreviousHash),
+			TransCnt:           uint64(transCnt),
 		},
 	}
 	zclog.Debugf("区块编号: %d, 交易数量: %d", blockInfo.BlockNum, blockInfo.TransCnt)
